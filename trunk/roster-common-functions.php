d09afaf2e2b07d2e20a5e7c7da7b9ed32c3e0b55
mark <mark@96c4993e-0f1d-2d11-6717-7f96f89844b4>
2013-06-29 23:07:11 +0000

    Allowed user to change sort order on Roster page. Right now, they can sort by (last) name or gender.

--- trunk/roster-common-functions.php
+++ trunk/roster-common-functions.php
@@ -19,8 +19,19 @@ function displayRosterTable()
 //TODO fix query below and delete this
 echo "<p>This only shows the roster of the Oddballs from Spring 2013 at Golf Links on Mondays.</p>";
 //END DEBUG
-	$db_query_result = mysqli_query($db_con, "SELECT playerID, firstName, lastName, shirtNumber, gender FROM players INNER JOIN teams ON players.associatedTeam = teams.teamID WHERE teamID = 1 ORDER BY lastName");
-	echo "<table><tr><th>Player Name</th><th>Number</th><th>Gender</th></tr>";
+
+//TODO is there a proper way to compare strings?
+//TODO how to stop warning when 'orderby' is not in URL?
+	if ($_GET['orderby'] == "name")
+		$orderBy = "lastName";
+	else
+		$orderBy = "gender";
+
+	$db_query_result = mysqli_query($db_con, "SELECT playerID, firstName, lastName, shirtNumber, gender FROM players INNER JOIN teams ON players.associatedTeam = teams.teamID WHERE teamID = 1 ORDER BY " . $orderBy);
+$thisFile = $_SERVER["PHP_SELF"];
+$parts = Explode('/', $thisFile);
+$thisFile = $parts[count($parts) - 1];
+	echo "<table><tr><th><a href=\"" . $thisFile . "?orderby=name\">Player Name</a></th><th>Number</th><th><a href=\"" . $thisFile . "?orderby=gender\">Gender</a></th></tr>";
 //DEBUG
 // show an error if the query failed
 if ($db_query_result == NULL)
@@ -29,6 +40,7 @@ if ($db_query_result == NULL)
 }
 //END DEBUG
 
+	$total = 0;
 	// iterate through each row of the result, use a background color based on gender
 	while ($row = mysqli_fetch_array($db_query_result))
 	{
@@ -42,8 +54,12 @@ if ($db_query_result == NULL)
 			echo "<tr class=\"gender-missing\">";
 //TODO need to encode player name as GET or POST data below
 		echo "<td><a href=\"player-profile.php?id=" . $row['playerID'] . "\">" . $row['firstName'] . " " . $row['lastName'] . "</a></td><td>" . $row['shirtNumber'] . "</td><td>" . $row['gender'] . "</td></tr>";
+
+		$total++;
 	}
 	echo "</table>";
+
+	echo "<p class=\"bold\">Total: " . $total . "</p>";
 
 //TODO Do I really *not* have to close the connection?
 	mysqli_close($db_con);
