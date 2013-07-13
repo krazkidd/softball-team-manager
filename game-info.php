2126e42ad4fcb835d0acf5541e17a187fb7cdec5
mark <mark@96c4993e-0f1d-2d11-6717-7f96f89844b4>
2013-07-13 05:24:27 +0000

    Game Info page seems to be working except it shows team IDs instead of team names right now.

--- game-info.php
+++ game-info.php
@@ -47,7 +47,7 @@ if ($db_game_info_query_result == NULL)
 }
 if ($db_team_info_query_result == NULL)
 {
-  echo "<p class=\"db-error\">The game info result was NULL :(</p>";
+  echo "<p class=\"db-error\">The team info result was NULL :(</p>";
 }
 //END DEBUG
 
@@ -59,12 +59,13 @@ if ($db_team_info_query_result == NULL)
 	}
 	else if (isset($date))
 	{
-		$db_query_result = mysqli_query($db_con, "SELECT * FROM games JOIN teams AS t1 ON games.homeTeam = t1.teamID JOIN teams as t2 ON games.visitingTeam = t2.teamID WHERE date = '" . date("Y-m-d", $date) . "' ORDER BY time");
+		// this is not the same game info query as above, since this grabs all games for a single date
+		$db_game_info_query_result = mysqli_query($db_con, "SELECT * FROM games JOIN teams AS t1 ON games.homeTeam = t1.teamID JOIN teams as t2 ON games.visitingTeam = t2.teamID WHERE date = '" . date("Y-m-d", $date) . "' ORDER BY time");
 //DEBUG
 // show an error if the query failed
-if ($db_query_result == NULL)
+if ($db_game_info_query_result == NULL)
 {
-  echo "<p class=\"db-error\">The date query result was NULL :(</p>";
+  echo "<p class=\"db-error\">The game info query result was NULL :(</p>";
 }
 //END DEBUG
 
@@ -94,10 +95,16 @@ function getDayFromMySQLDate($dateString)
 	return substr($dateString, 8, 2);
 }
 
+//TODO add second parameter for date?
+function mktimeFromMySQLTime($timeString)
+{
+	return mktime(getHourFromMySQLTime($timeString), getMinuteFromMySQLTime($timeString));
+}
 
-//TODO need ID for table
+//TODO I think i would like a better ID for this div, and i would especially like to merge it's style with the case where a game ID is present
+		echo "<div id=\"game-list\">";
 		echo "<table><tr><th>Time</th><th>Home Team</th><th>Visiting Team</th>";
-		$row = mysqli_fetch_array($db_query_result);
+		$row = mysqli_fetch_array($db_game_info_query_result);
 		// if date/time of first game + 1 hour is passed, show result columns
 //TODO i guess there might be a problem comparing time and mktime values. see gmmktime doc page
 		$showResults = false;
@@ -115,15 +122,25 @@ function getDayFromMySQLDate($dateString)
 			$parts = Explode('/', $thisFile);
 			$thisFile = $parts[count($parts) - 1];
 //TODO make every row element a link?
-			echo "<tr><td><a href=\"" . $thisFile . "?gameid={$row['gameId']}\">{$row['time']}</a></td><td>{$row['homeTeam']}</td><td>{$row['visitingTeam']}</td>";
+//TODO this isn't working. right now, i can get the team names from the one query but i have to use a numerical index, which can change... Either fix the original query to fix that,
+// or fix pulling the team names from the teams table
+			// get team names
+			//$tmpArray = mysqli_fetch_array(mysqli_query($db_con, "SELECT * FROM teams WHERE teamID = {$row['homeTeam']}"));
+			//$homeTeam = $tmpArray['homeTeam'];
+			//$visitingTeam = mysqli_fetch_array(mysqli_query($db_con, "SELECT * FROM teams WHERE teamID = {$row['visitingTeam']}"))['visitingTeam'];
+			echo "<tr><td><a href=\"" . $thisFile . "?gameid={$row['gameID']}\">" . date("g\:i a", mktimeFromMySQLTime($row['time'])) . "</a></td><td>{$row[9]}</td><td>{$row[12]}</td>";
 
 			if ($showResults)
-				echo "<td>{$row['finalHomeScore']}</td><td>{$row['finalVisitingScore']}</td>";
-
+			{
+				$finalHome = $row['finalHomeScore'];
+				$finalAway = $row['finalVisitingScore'];
+				echo "<td>{$finalHome}" . ($finalHome > $finalAway ? "&nbsp;<img alt=\"Winner\" src=\"icons/1373708645_trophy.png\" />" : "") . "</td><td>{$finalAway}" . ($finalAway > $finalHome ? "&nbsp;<img alt=\"Winner\" src=\"icons/1373708645_trophy.png\" />" : "") . "</td>";
+			}
 			echo "</tr>";
-		}  while ($row = mysqli_fetch_array($db_query_result));
+		}  while ($row = mysqli_fetch_array($db_game_info_query_result));
 
 		echo "</table>";
+		echo "</div> <!-- game-list -->";
 	}
 	else
 //TODO don't use die(), and close the db if needed
