7dbd200172432317ae93357796c8eb3d6c865701
mark <mark@96c4993e-0f1d-2d11-6717-7f96f89844b4>
2013-07-17 04:04:28 +0000

    Moved code that prints calendar to a common file and added ability to select any month from URL. I fixed a bug that wasn't correctly determining if a date was passed or not.

--- calendar-common-functions.php
+++ calendar-common-functions.php
@@ -1,31 +1,164 @@
 <?php
 
-	function getHourFromMySQLTime($timeString)
-	{
-		return substr($timeString, 0, 2);
-	}
-	function getMinuteFromMySQLTime($timeString)
-	{
-		return substr($timeString, 3, 2);
-	}
-	function getYearFromMySQLDate($dateString)
-	{
-		return substr($dateString, 0, 4);
-	}
-	function getMonthFromMySQLDate($dateString)
-	{
-		return substr($dateString, 5, 2);
-	}
-	function getDayFromMySQLDate($dateString)
-	{
+require_once("common-functions.php");
+
+function getHourFromMySQLTime($timeString)
+{
+	return substr($timeString, 0, 2);
+}
+function getMinuteFromMySQLTime($timeString)
+{
+	return substr($timeString, 3, 2);
+}
+function getYearFromMySQLDate($dateString)
+{
+	return substr($dateString, 0, 4);
+}
+function getMonthFromMySQLDate($dateString)
+{
+	return substr($dateString, 5, 2);
+}
+function getDayFromMySQLDate($dateString)
+{
 //TODO check for NULL argument?
-		return substr($dateString, 8, 2);
-	}
+	return substr($dateString, 8, 2);
+}
 
 //TODO add second parameter for date?
-	function mktimeFromMySQLTime($timeString)
+function mktimeFromMySQLTime($timeString)
+{
+	return mktime(getHourFromMySQLTime($timeString), getMinuteFromMySQLTime($timeString));
+}
+
+function displayCalendar($month, $year)
+{
+//TODO make sure month and year are valid (even go so far as to check db?)
+	$db_con = connectToDB();
+	
+//TODO select ALL games for a team, or just for one league, depending on user preference
+//TODO will MySQL understand if month doesn't have leading zeros? NOPE.
+	$gameList = mysqli_query($db_con, "SELECT date FROM games JOIN leagues ON associatedLeague = leagueID JOIN seasons ON leagues.associatedSeason = seasons.seasonID WHERE games.date LIKE '$year-$month-%' GROUP BY date ORDER BY date");
+
+	// get the time for the 1st of the month
+	$timeOfFirstDay = mktime(0, 0, 0, $month, 1, $year);
+
+	// get name of the month
+	$monthName = date('F', $timeOfFirstDay);
+
+	// get the day of week the 1st falls on
+	$dayOfWeek = date('D', $timeOfFirstDay);
+
+	// how many blank days to draw in calendar grid before the 1st
+	switch($dayOfWeek)
 	{
-		return mktime(getHourFromMySQLTime($timeString), getMinuteFromMySQLTime($timeString));
+		case 'Sun':
+			$numBlankDays = 0;
+			break;
+		case 'Mon':
+			$numBlankDays = 1;
+			break;
+		case 'Tue':
+			$numBlankDays = 2;
+			break;
+		case 'Wed':
+			$numBlankDays = 3;
+			break;
+		case 'Thu':
+			$numBlankDays = 4;
+			break;
+		case 'Fri':
+			$numBlankDays = 5;
+			break;
+		default:
+			$numBlankDays = 6;
+	}
+
+	// get number of days in month
+	$numDaysInMonth = date('t', $timeOfFirstDay);
+//TODO get days in previous month. PROBLEM: what if prev month is Dec? SOLUTION: just subtract 24 hours from $timeOfFirstDay
+	//$numDaysInPrevMonth = date('t', mktime(0, 0, 0, $month
+
+	// print the calendar table
+?>
+			<table>
+				<tr>
+					<th colspan="7"><?= $monthName ?> <?= $year ?></th>
+				</tr>
+				<tr>
+					<td><a href="calendar.php?view=daily&amp;day=sun">Sun</a></td>
+					<td><a href="calendar.php?view=daily&amp;day=mon">Mon</a></td>
+					<td><a href="calendar.php?view=daily&amp;day=tue">Tue</a></td>
+					<td><a href="calendar.php?view=daily&amp;day=wed">Wed</a></td>
+					<td><a href="calendar.php?view=daily&amp;day=thu">Thu</a></td>
+					<td><a href="calendar.php?view=daily&amp;day=fri">Fri</a></td>
+					<td><a href="calendar.php?view=daily&amp;day=sat">Sat</a></td>
+				</tr>
+				<tr>
+<?php
+	// keep track of the day of the week
+	$weeklyDayCount = 1;
+	// print blank days
+	while ($numBlankDays > 0)
+	{
+?>
+					<td>&nbsp;</td>
+<?php
+		$numBlankDays--;
+		$weeklyDayCount++;
+	}
+
+	// print the days of the month
+	$monthlyDayCount = 1;
+//TODO need to check that this is not null!
+	$gameRow = mysqli_fetch_array($gameList);
+	while ($monthlyDayCount <= $numDaysInMonth)
+	{
+		$gameDate = NULL;
+		$elementClass = NULL;
+
+		// if there is/was a game on this date, link to it and set the next game date to check for
+		if ($monthlyDayCount == getDayFromMySQLDate($gameRow['date']))
+		{
+			$elementClass = "gameday";
+			$gameDate = $gameRow['date'];
+			$gameRow = mysqli_fetch_array($gameList);
+		}
+		// but if the date has already passed, don't put a cute image
+		if (mktime(0, 0, 0, $month, $monthlyDayCount + 1, $year) < time())
+			$elementClass = "calDatePassed";
+		else if ($monthlyDayCount == date("d"))
+			$elementClass = "todays-date"
+
+//TODO how do I want the date to show in the URL (i.e. with or without dashes?), and do dashes need to be escaped?
+?>
+					<td<?= $elementClass != NULL ? " class=\"$elementClass\"" : "" ?>><?= $gameDate != NULL ? "<a href=\"calendar.php?date={$gameDate}\">$monthlyDayCount</a>" : $monthlyDayCount ?></td>
+<?php		
+		$monthlyDayCount++;
+		
+		$weeklyDayCount++;
+		if ($weeklyDayCount > 7)
+		{
+?>
+				</tr>
+				<tr>
+<?php
+			$weeklyDayCount = 1;
+		}
+	}
+
+	while ($weeklyDayCount <= 7)
+	{
+?>
+					<td>&nbsp;</td>
+<?php
+		$weeklyDayCount++;
 	}
 
 ?>
+				</tr>
+			</table>
+<?php
+}
+
+?>
+
--- calendar.php
+++ calendar.php
@@ -145,126 +145,22 @@ echo "<p>{$_GET['date']}</p>";
 	}
 	else
 	{
-		// print this month's calendar
-//		$currentDate = time();
-//TODO change this back to current time (above line)
-//TODO this shouldn't really be called currentDate but rather default date (or date given by argument)
-//     Or target date or something. This gets *compared* against the current date. Maybe this should just be target MONTH???
-		$currentDate = mktime(0, 0, 0, 5, 1, 2013);
-
-		$dayPart = date('d', $currentDate);
-		$monthPart = date('m', $currentDate);
-		$yearPart = date('Y', $currentDate);
-
-//TODO select ALL games for a team, or just for one league, depending on user preference
-//TODO better query result var name
-		$db_query_result = mysqli_query($db_con, "SELECT gameID, date FROM games JOIN leagues ON associatedLeague = leagueID JOIN seasons ON leagues.associatedSeason = seasons.seasonID WHERE games.date LIKE '$yearPart-$monthPart-%' GROUP BY date ORDER BY date");
-
-		// get the 1st of the month
-		$firstDay = mktime(0, 0, 0, $monthPart, 1, $yearPart);
-
-		// get name of month
-		$monthName = date('F', $firstDay);
-
-		// get the day of week the 1st falls on
-		$dayOfWeek = date('D', $firstDay);
-
-		// how many blank days to draw in calendar grid before the 1st
-		switch($dayOfWeek)
+//TODO check input for validity...month var must have leading zero, for example
+		if (isset($_GET['month']))
 		{
-			case 'Sun':
-				$numBlankDays = 0;
-				break;
-			case 'Mon':
-				$numBlankDays = 1;
-				break;
-			case 'Tue':
-				$numBlankDays = 2;
-				break;
-			case 'Wed':
-				$numBlankDays = 3;
-				break;
-			case 'Thu':
-				$numBlankDays = 4;
-				break;
-			case 'Fri':
-				$numBlankDays = 5;
-				break;
-			default:
-				$numBlankDays = 6;
+			$month = $_GET['month'];
+			if (isset($_GET['year']))
+				$year = $_GET['year'];
+			else
+				$year = date("Y");
+		}
+		else
+		{
+			$month = date("m");
+			$year = date("Y");
 		}
 
-		// get number of days in month
-		$numDaysInMonth = date('t', $currentDate);
-//TODO print days in previous month. PROBLEM: what if prev month is Dec?
-		//$numDaysInPrevMonth = date('t', mktime(0, 0, 0, $month
-
-//TODO what do I want to link to in the calendar? should every day be a link to a search for games on that date? or should i only link to games on that date? because i don't want to do 30 queries...right? (if ever day is a link, highlight days with games.) or i can link only game days (but that hardly stops the queries unless I save them in an array...which is exactly what i do...)
-
-		// print the calendar
-		echo "<table>\n<tr><th colspan=\"7\">$monthName $yearPart</th></tr>\n<tr>";
-		echo "<td><a href=\"calendar.php?view=daily&day=sun\">Sun</a></td>";
-		echo "<td><a href=\"calendar.php?view=daily&day=mon\">Mon</a></td>";
-		echo "<td><a href=\"calendar.php?view=daily&day=tue\">Tue</a></td>";
-		echo "<td><a href=\"calendar.php?view=daily&day=wed\">Wed</a></td>";
-		echo "<td><a href=\"calendar.php?view=daily&day=thu\">Thu</a></td>";
-		echo "<td><a href=\"calendar.php?view=daily&day=fri\">Fri</a></td>";
-		echo "<td><a href=\"calendar.php?view=daily&day=sat\">Sat</a></td>";
-		echo "</tr>\n";
-
-		echo "<tr>";
-
-		// keep track of the day of the week
-		$weeklyDayCount = 1;
-		// print blank days
-		while ($numBlankDays > 0)
-		{
-			echo "<td>&nbsp;</td>";
-			$numBlankDays--;
-			$weeklyDayCount++;
-		}
-
-		// print the days of the month
-		$monthlyDayCount = 1;
-//TODO need to check that this is not null!
-		$gameDateRow = mysqli_fetch_array($db_query_result);
-		while ($monthlyDayCount <= $numDaysInMonth)
-		{
-			$gameDate = NULL;
-			$elementClass = NULL;
-
-			// if there is/was a game on this date, link to it and set the next game date to check for
-			if ($monthlyDayCount == getDayFromMySQLDate($gameDateRow['date']))
-			{
-				$elementClass = "gameday";
-				$gameDate = $gameDateRow['date'];
-				$gameDateRow = mysqli_fetch_array($db_query_result);
-			}
-			// but if the date has already passed, don't put a cute image
-			if (mktime(0, 0, 0, $monthPart, $dayPart + 1, $yearPart) < time())
-				$elementClass = "calDatePassed";
-
-//TODO how do I want the date to show in the URL (i.e. with or without dashes?), and do dashes need to be escaped?
-			echo "<td" . ($elementClass != NULL ? " class=\"$elementClass\"" : "") . ">" . ($gameDate != NULL ? "<a href=\"calendar.php?date={$gameDate}\">$monthlyDayCount</a>" : $monthlyDayCount) . "</td>";
-			
-			$monthlyDayCount++;
-			
-			$weeklyDayCount++;
-			if ($weeklyDayCount > 7)
-			{
-				echo "</tr>\n<tr>";
-				$weeklyDayCount = 1;
-			}
-		}
-
-		while ($weeklyDayCount <= 7)
-		{
-			echo "<td>&nbsp;</td>";
-			$weeklyDayCount++;
-		}
-
-		// close table
-		echo "</tr>\n</table>";
+		displayCalendar($month, $year);
 	}
 
 	closeDB($db_con);
