4781c81913d64d0a1c3e332c4de7c125bebb936d
mark <mark@96c4993e-0f1d-2d11-6717-7f96f89844b4>
2013-07-18 19:21:36 +0000

    Changed the front page to check if the user is logged in or not and display different things depending on that (e.g. only show link to Calendar/Roster if logged in). Also started a login module while will link to the login page if user is not logged in or show the some user info and a logout link if the user is logged in.

---
+++ trunk/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+	<head>
+	<title>Team Manager</title>
+	<meta http-equiv="content-type" 
+		content="text/html;charset=utf-8" />
+	<link rel="stylesheet" type="text/css" href="style.css" /> 
+<?php
+	require_once("common-definitions.php");
+?>
+	</head>
+
+	<body id="index-body">
+
+		<div id="index-header">
+		      <h1>Team Manager</h1>
+		</div>
+      
+		<div id="index-content">
+<?php
+	if (isLoggedIn())
+	{
+?>
+			<p><a href="roster.php">Go to Roster &gt; &gt;</a><br />
+			<a href="calendar.php">Go to Calendar &gt; &gt;</a></p>
+<?php
+	}
+	else
+	{
+?>
+			<p>You are not logged in.</p>
+<?php
+	}
+?>
+		</div>
+<?
+
+<?php
+	include("login-module.php");
+?>
+
+		<div id="index-footer">
+			<p><a href="index.php">Home</a></p>
+		</div>
+
+	</body>
+</html>
