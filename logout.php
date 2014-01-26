47d776a8ee1a22444efc3713a0907322909382e3
mark <mark@96c4993e-0f1d-2d11-6717-7f96f89844b4>
2014-01-26 02:03:36 +0000

    Changed Login button style for login module.

--- logout.php
+++ logout.php
@@ -5,51 +5,61 @@
 
 	if (isLoggedIn())
 	{
-//TODO do some testing for how to correctly kill the session, because if the user still has a copy of the session id, the server may still remember the session
 		$_SESSION = array(); // or session_unset()
 		session_destroy();
 	}
 	else
 	{
-//TODO if i don't want a time delay, and I *do* for mobile users, put a message on the home screen about the last action--successful or not
-		header("Location: index.php");
+		// user not logged in, redirect to Home page
+		header('Location: index.php');
+		exit(0);
 	}
+
+	require_once("common-definitions.php");
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml">
-
 	<head>
-	<title>Logout</title>
+	<title>Logout - Team Manager</title>
 	<meta http-equiv="content-type" 
 		content="text/html;charset=utf-8" />
-	<link rel="stylesheet" type="text/css" href="style.css" /> 
+	<link rel="stylesheet" type="text/css" href="styles/style.css" /> 
 	</head>
 
 	<body id="logout-body">
+		<div id="container">
+			<div id="logout-header">
+			      <h1>Logout</h1>
+			</div>
 
-		<div id="logout-header">
-		      <h1>Logout</h1>
-		</div>
-      
-		<div id="logout-content">
 <?php
-	if (!isLoggedIn())
+	include("includes/navbar.php");
+?>
+
+			<div id="logout-content">
+<?php
+	if ( !isLoggedIn())
 	{
 ?>
-			<p>You were successfully logged out.<br>
+				<p>You were successfully logged out.<br />
 <?php
 	}
 	else
 	{
 ?>
-			<p>You were not logged in. You are automatically being redirected to the home page.<br>
+//TODO this should never be seen. Show error message instead? (No, there's nothing the user can do to fix the error, so don't just show a message.)
+				<p>You were not logged in. You are automatically being redirected to the home page.<br />
 <?php
 	}
 ?>
-			Click <a href="index.php">here</a> to go to the home page.</p>
-		</div> <!-- logout-content -->
+				   Click <a href="index.php">here</a> to go to the home page.</p>
+			</div> <!-- logout-content -->
 
+<?php
+	include("includes/footer.php");
+?>
+		</div> <!-- end container -->
 	</body>
 </html>
