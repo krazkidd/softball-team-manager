5b2ee2460f5ce20764e9167d84656e331b485f66
mark <mark@96c4993e-0f1d-2d11-6717-7f96f89844b4>
2014-01-27 04:19:39 +0000

    Updated Player Profile page to new style but player info doesn't display due to DB schema change.

--- player-profile.php
+++ player-profile.php
@@ -1,28 +1,46 @@
+<?php
+	session_start();
+	require_once('common-definitions.php');
+
+	if (!isLoggedIn())
+	{
+		header('Location: index.php');
+		quit(0);
+	}
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-	<title>Player Profile</title>
+	<title>Player Profile - Team Manager</title>
 	<meta http-equiv="content-type" 
 		content="text/html;charset=utf-8" />
-	<link rel="stylesheet" type="text/css" href="style.css" /> 
+	<link rel="stylesheet" type="text/css" href="styles/style.css" /> 
 <?php
-	require_once("player-common-functions.php");
+	require_once('player-common-functions.php');
 ?>
 	</head>
 
 	<body id="player-profile-body">
-		<div id="player-profile-header">
-			<h1>Player Profile</h1>
-		</div> <!-- player-profile-header -->
+		<div id="container">
+			<div id="player-profile-header">
+				<h1>Player Profile</h1>
+			</div> <!-- player-profile-header -->
 
-		<div id="player-profile-content">
+<?php
+	include('includes/navbar.php');
+?>
+
+			<div id="player-profile-content">
 <?php
 
 	displayPlayerInfo($_GET['id']);
 
+
+	include('includes/footer.php');
 ?>
-		</div> <!-- player-profile-content -->
+			</div> <!-- player-profile-content -->
+		</div> <!-- end container -->
 	</body>
 </html>
