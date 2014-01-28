50fa50c1d26b77bfd4f0213be800e65a3ca278c8
mark <mark@96c4993e-0f1d-2d11-6717-7f96f89844b4>
2014-01-28 19:27:15 +0000

    Removed some echo statements from old code.

--- trunk/player-common-functions.php
+++ trunk/player-common-functions.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once("common-definitions.php");
+require_once('common-definitions.php');
 
 function formatPhoneNumber($phoneStr)
 {
@@ -16,61 +16,27 @@ function formatPhoneNumber($phoneStr)
 }
 
 //TODO this function is not being used--I might just turn into a module
+//     How do I turn it into a module? I want to be able to show any player's info, but i need to be able to pass a parameter to do that
 function displayPlayerInfo($playerID)
 {
 	$db_con = connectToDB();
 
-	$db_query_result = mysqli_query($db_con, "SELECT firstName, lastName, phoneNumber, emailAddress, shirtNumber, gender FROM players WHERE players.playerID = $playerID");
-//DEBUG
-// show an error if the query failed
-if ($db_query_result == NULL)
-{
-  echo "<p class=\"db-error\">The result was NULL :(</p>";
-}
-//END DEBUG
+	$db_query_result = mysqli_query($db_con, "SELECT * FROM Player WHERE Player.ID = '$playerID'");
 
-	$playerInfo = mysqli_fetch_array($db_query_result);
-
-	echo "<img title=\"" . $playerInfo['firstName'] . " " . $playerInfo['lastName'] . "\" src=\"images/player-no-image.gif\" />";
-	echo "<h2 id=\"player-name-header\">" . $playerInfo['firstName'] . " " . $playerInfo['lastName'] . "</h2>";
-	echo "<h3>#" . $playerInfo['shirtNumber'] . "</h3>";
-	echo "<p><span class=\"bold\">Phone:</span> " . $playerInfo['phoneNumber'] . "<br>";
-	echo "<span class=\"bold\">Email:</span> " . $playerInfo['emailAddress'] . "<br>";
-	echo "<span class=\"bold\">Gender:</span> " . $playerInfo['gender'] . "</p>";
-
-	// print current/most recent teams in a table
-	echo "<div id=\"player_s-teams\">";
-	// get today's date	
-	//$today = date("Y-m-d");
-	// find all rows for the player in the players table. Players are duplicated as seasons pass; that's why this query is ugly.
-//TODO need a better way to find the player. Now, we are only checking first and last name. That is not unique enough. Is it okay to keep driver's license #? How about just a hash?
-	$db_query_result = mysqli_query($db_con, "SELECT * FROM players AS p1 JOIN players AS p2 ON p1.firstName = p2.firstName AND p1.lastName = p2.lastName JOIN teams ON p2.associatedTeam = teams.teamID JOIN leagues ON teams.associatedLeague = leagues.leagueID JOIN seasons ON leagues.associatedSeason = seasonID WHERE p1.playerID = " . $playerID);
-	$tableHeaderMustBePrinted = true;
-	while ($row = mysqli_fetch_array($db_query_result))
+	if ($db_query_result)
 	{
-//TODO it might be best if I just have a global variable for the current/next season
-//Previously, I wanted to have a function to find the latest season the player was playing in. I can still do that.
-		if ($row['seasonID'] == 1)
-		{
-			if ($tableHeaderMustBePrinted)
-			{
-				$tableHeaderMustBePrinted = false;		
-				echo "<p class=\"bold\">Currently playing in these divsions:</p>";
-				echo "<table><tr><th>Season</th><th>Division</th><th>Class</th><th>Park</th><th>Team</th></tr>";
-			}
+		$playerInfo = mysqli_fetch_array($db_query_result);
+?>
+<div id="player-module">
+	<img title="<?= $playerInfo['FirstName'] . " " . $playerInfo['LastName'] ?>" src="/images/player-no-image.gif" />
+	<h2 id="player-name-header"><?= $playerInfo['FirstName'] . " " . $playerInfo['LastName'] ?></h2>
+	<!-- TODO show nickname? -->
+	<!-- <h3>#</h3> TODO shirt number relies on team/league/etc. -->
 
-//TODO needs a day-of-week column
-			echo "<tr><td class=\"spring13\">" . $row['description'] . "</td><td>" . $row['division'] . "</td><td>" . $row['class'] . "</td><td>" . $row['park'] . "</td><td>" . $row['name'] . "</td></tr>";
-		}
+</div> <!-- player-module -->
+<?php
 	}
-
-	// only close table if needed
-	if ( !$tableHeaderMustBePrinted)
-		echo "</table>";
-
-	echo "</div> <!-- player_s-teams -->";
 
 	closeDB($db_con);
 }
-
 ?>
