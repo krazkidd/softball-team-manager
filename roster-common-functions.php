ecaf8056c8027effbf6589ec042833c383f7fc32
mark <mark@96c4993e-0f1d-2d11-6717-7f96f89844b4>
2014-01-31 06:54:56 +0000

    Roster page loads but it doesn't actually link to or show any rosters. I left it broken because I'm about to refactor to an MVC-style site layout.

--- roster-common-functions.php
+++ roster-common-functions.php
@@ -1,67 +1,64 @@
 <?php
-//DEBUG
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-//END DEBUG
+	require_once('common-definitions.php');
 
-require_once("common-definitions.php");
+/*ERROR this function cannot work with just a team ID.
+also, I'm not sure about this idea of printing (tables)
+from a function
+FROM Apache PHP style guide:
+Separate business logic, data, and presentational layers. In other words, keep the Model-View-Controller pattern in mind. It is very handy and can make PHP development a lot easier.
+https://svn.apache.org/repos/asf/shindig/trunk/php/docs/style-guide.html*/
 
-function displayRosterTable($teamID)
-{
-	$db_con = connectToDB();
+	function displayRosterTable($teamID)
+	{
+		$db_con = connectToDB();
 
-	// show team info
-	$db_team_info_query_result = mysqli_query($db_con, "SELECT * FROM teams JOIN leagues ON teams.associatedLeague = leagues.leagueID JOIN seasons ON leagues.associatedSeason = seasons.seasonID WHERE teams.teamID = $teamID");
-	$gameInfo = mysqli_fetch_array($db_team_info_query_result);
+		// show team info
+		$db_team_info_query_result = mysqli_query($db_con, "SELECT * FROM teams JOIN leagues ON teams.associatedLeague = leagues.leagueID JOIN seasons ON leagues.associatedSeason = seasons.seasonID WHERE teams.teamID = $teamID");
+		$gameInfo = mysqli_fetch_array($db_team_info_query_result);
 ?>
-	<h3><?= $gameInfo["name"] ?></h3>
-	<p><?= $gameInfo["description"] ?><br>
-	<?= $gameInfo["division"] . " " . $gameInfo["class"] . " @ " . $gameInfo["park"] . " #" . $gameInfo["field"] . " on " . $gameInfo["dayOfWeek"] ?></p>
+		<h3><?= $gameInfo["name"] ?></h3>
+		<p><?= $gameInfo["description"] ?><br>
+		<?= $gameInfo["division"] . " " . $gameInfo["class"] . " @ " . $gameInfo["park"] . " #" . $gameInfo["field"] . " on " . $gameInfo["dayOfWeek"] ?></p>
 <?php
 //TODO is there a proper way to compare strings? == vs. === vs. strcmp()
-	if (isset($_GET['orderby']) && $_GET['orderby'] == "name")
-		$orderBy = "lastName";
-	else
-		$orderBy = "gender, lastName";
+		if (isset($_GET['orderby']) && $_GET['orderby'] == "name")
+			$orderBy = "lastName";
+		else
+			$orderBy = "gender, lastName";
 
-	$db_roster_query_result = mysqli_query($db_con, "SELECT playerID, firstName, lastName, shirtNumber, gender FROM players INNER JOIN teams ON players.associatedTeam = teams.teamID WHERE teamID = $teamID ORDER BY " . $orderBy);
+		$db_roster_query_result = mysqli_query($db_con, "SELECT playerID, firstName, lastName, shirtNumber, gender FROM players INNER JOIN teams ON players.associatedTeam = teams.teamID WHERE teamID = $teamID ORDER BY " . $orderBy);
 
-	/*$thisFile = $_SERVER["PHP_SELF"];
-	$parts = Explode('/', $thisFile);
-	$thisFile = $parts[count($parts) - 1];
-	echo "<table><tr><th><a href=\"" . $thisFile . "?orderby=name\">Player Name</a></th><th>Number</th><th><a href=\"" . $thisFile . "?orderby=gender\">Gender</a></th></tr>";*/
-	echo "<table><tr><th>Player Name</th><th>Number</th><th>Gender</th></tr>";
+		/*$thisFile = $_SERVER["PHP_SELF"];
+		$parts = Explode('/', $thisFile);
+		$thisFile = $parts[count($parts) - 1];
+		echo "<table><tr><th><a href=\"" . $thisFile . "?orderby=name\">Player Name</a></th><th>Number</th><th><a href=\"" . $thisFile . "?orderby=gender\">Gender</a></th></tr>";*/
+		echo "<table><tr><th>Player Name</th><th>Number</th><th>Gender</th></tr>";
 //DEBUG
-// show an error if the query failed
-if ($db_roster_query_result == NULL)
-{
-  echo "<p class=\"db-error\">The roster result was NULL :(</p>";
-}
+if ( !$db_roster_query_result)
+  error_log('The roster result was NULL');
 //END DEBUG
 
-	$total = 0;
-	// iterate through each row of the result, use a background color based on gender
-	while ($playerRow = mysqli_fetch_array($db_roster_query_result))
-	{
-		$gender = $playerRow['gender'];
+		$total = 0;
+		// iterate through each row of the result, use a background color based on gender
+		while ($playerRow = mysqli_fetch_array($db_roster_query_result))
+		{
+			$gender = $playerRow['gender'];
 
-                if ($gender == 'M')
-			echo "<tr class=\"gender-male\">";
-		else if ($gender == 'F')
-			echo "<tr class=\"gender-female\">";
-		else
-			echo "<tr class=\"gender-missing\">";
+			if ($gender == 'M')
+				echo "<tr class=\"gender-male\">";
+			else if ($gender == 'F')
+				echo "<tr class=\"gender-female\">";
+			else
+				echo "<tr class=\"gender-missing\">";
 //TODO need to encode player name as GET or POST data below
-		echo "<td><a href=\"player-profile.php?id=" . $playerRow['playerID'] . "\">" . $playerRow['firstName'] . " " . $playerRow['lastName'] . "</a></td><td>" . $playerRow['shirtNumber'] . "</td><td>" . $playerRow['gender'] . "</td></tr>";
+			echo "<td><a href=\"player-profile.php?id=" . $playerRow['playerID'] . "\">" . $playerRow['firstName'] . " " . $playerRow['lastName'] . "</a></td><td>" . $playerRow['shirtNumber'] . "</td><td>" . $playerRow['gender'] . "</td></tr>";
 
-		$total++;
+			$total++;
+		}
+		echo "</table>";
+
+		echo "<p class=\"bold\">Total: " . $total . "</p>";
+
+		closeDB($db_con);
 	}
-	echo "</table>";
-
-	echo "<p class=\"bold\">Total: " . $total . "</p>";
-
-	closeDB($db_con);
-}
-
 ?>
-
--- roster.php
+++ roster.php
@@ -7,19 +7,18 @@
 		header('Location: index.php');
 		quit(0);
 	}
+
+	require_once('roster-common-functions.php');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-	<title>Roster - Team Manager</title>
-	<meta http-equiv="content-type" 
-		content="text/html;charset=utf-8" />
-	<link rel="stylesheet" type="text/css" href="styles/style.css" /> 
-<?php
-	require_once('roster-common-functions.php');
-?>
+		<title>Roster - Team Manager</title>
+		<meta http-equiv="content-type" 
+			content="text/html;charset=utf-8" />
+		<link rel="stylesheet" type="text/css" href="styles/style.css" /> 
 	</head>
 
 	<body id="roster-body">
@@ -32,37 +31,36 @@
 	include('includes/navbar.php');
 ?>
 
-			<div id="roster">
-<p>BROKEN</p>
+			<div id="roster-content">
 <?php
-	/*$db_con = connectToDB();
-	// get this user's teams for the preferred season
-//TODO the teams table should have a manager column that is a foreign key to the user/login table. then i could get all teams with the manager who is the currently logged-in user
-//TODO need a way to order by dayOfWeek, but not alphabetically... Can I specify an order?
-	$db_team_query_result = mysqli_query($db_con, "SELECT teamID FROM teams JOIN leagues ON teams.associatedLeague = leagues.LeagueID JOIN seasons ON leagues.associatedSeason = seasons.seasonID WHERE name = '" . getUserTeamName() . "' AND seasonID = '" . getUserSeasonID() . "'");
-//DEBUG
-if ($db_team_query_result == NULL)
-	echo "<p class=\"db-error\">Team query result was NULL :(</p>";
-//END DEBUG
-
-	while ($teamRow = mysqli_fetch_array($db_team_query_result))
+	if (isset($_GET['name']))
 	{
-		displayRosterTable($teamRow['teamID']);
-	}
+		$db_con = connectToDB();
 
-	closeDB($db_con);*/
+		$escapedTeamName = mysqli_real_escape_string($db_con, $_GET['name']);
+		$roster_query_result = mysqli_query($db_con, "SELECT DISTINCT TeamName, ParkName, FieldNum, DayOfWeek, SeasonDescription FROM Roster WHERE TeamName = '$escapedTeamName' ORDER BY SeasonDescription");
+//DEBUG
+if ( !$roster_query_result)
+	error_log("Roster query result was NULL");
+//END DEBUG
 ?>
-			</div> <!-- roster -->
+				<ul>
+<?php
+		while ($leagueRow = mysqli_fetch_array($roster_query_result))
+		{
+?>
+					<!-- TODO fix this: <li><a href="roster.php?name=<? $_GET['name'] ?>& -->
+					<li><a href="#">TODO</a></li>
+<?php
+		}
+?>
+				</ul>
 
-			<!-- <div id="form-edit-roster">
-				<form action="edit-roster.php" method="post">
-					COMMENTOUT <input type="submit" name="addplayer" value="Add Player" />
-					<input type="submit" name="removeplayer" value="Remove Player" /> COMMENTOUT
-					<input type="submit" name="edit" value="Edit Roster" />
-				</form>
-			</div> --> <!-- form-edit-roster -->
-
-			<p><a href="edit-roster.php">Edit Roster &gt; &gt;</a></p>
+<?php
+		closeDB($db_con);
+	}
+?>
+			</div> <!-- roster-content -->
 
 <?php
 	include('includes/footer.php');
