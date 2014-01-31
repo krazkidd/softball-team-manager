97cff616c0b82f76714ebb1befa155ca17c19316
mark <mark@96c4993e-0f1d-2d11-6717-7f96f89844b4>
2014-01-31 19:40:46 +0000

    Fixed Manage page which I said was working in last commit.

--- branches/mvc/views/manage.php
+++ branches/mvc/views/manage.php
@@ -1,32 +1,24 @@
 <?php $title = 'Manage' ?>
 
-<?php
-echo 'in Manage view';
-	ob_start();
+<?php ob_start() ?>
 
-	if ($action == 'show-team')
-	{
-?>
-		<img title="<?= $teamInfo['TeamName'] ?>" src="images/team-no-image.png" />
-		<h2><span style="color: #<?= $teamInfo['PriColor'] ?>; background-color: #<?= $teamInfo['SecColor'] ?>"><?= $teamInfo['TeamName'] ?></span></h2>
-		<p><?= $teamInfo['Motto'] ?></p>
-		<p><a href="roster.php?name=<?= urlencode($teamInfo['TeamName']) ?>">View Rosters</a><br />
-		    <a href="#">View lineups for next games</a></p>
-<?php
-	}
-	else // $action == 'show-list'
-	{
-?>
-		<?php if ($managedTeamsList) { ?>
-			<p>Which team do you want to manage?</p>
-			<p>
-				<ul>
-					<?php foreach ($team as $managedTeamsList) { ?>
-						<li><a href="manage.php?name=<?= urlencode($team) ?>"><?= $team ?></a></li>
-					<?php } ?>
-				</ul>
-			</p>
-		<?php } else { ?>
+<?php if ($action == 'show-team') { ?>
+	<img title="<?= $teamInfo['TeamName'] ?>" src="images/team-no-image.png" />
+	<h2><span style="color: #<?= $teamInfo['PriColor'] ?>; background-color: #<?= $teamInfo['SecColor'] ?>"><?= $teamInfo['TeamName'] ?></span></h2>
+	<p><?= $teamInfo['Motto'] ?></p>
+	<p><a href="roster.php?name=<?= urlencode($teamInfo['TeamName']) ?>">View Rosters</a><br />
+	    <a href="#">View lineups for next games</a></p>
+<?php }	else { // $action == 'show-list' ?>
+	<?php if ($managedTeamsList) { ?>
+		<p>Which team do you want to manage?</p>
+		<p>
+			<ul>
+				<?php foreach ($team as $managedTeamsList) { ?>
+					<li><a href="manage.php?name=<?= urlencode($team) ?>"><?= $team ?></a></li>
+				<?php } ?>
+			</ul>
+		</p>
+	<?php } else { ?>
 			<p>You are not a manager of any teams.</p>
 		<?php } ?>
 <?php
@@ -35,4 +27,4 @@ echo 'in Manage view';
 	$content = ob_get_clean();
 ?>
 
-<?php require '/templates/layout.php' ?>
+<?php require 'templates/layout.php' ?>
