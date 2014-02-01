9535dfc3add4533101abcc2f1ce01f58110c72a0
mark <mark@96c4993e-0f1d-2d11-6717-7f96f89844b4>
2014-02-01 21:36:09 +0000

    Converted Roster page to MVC model. However, it's broken because it only takes a team name as a parameter when it needs season/league info, too. (It only works now because there's only one roster in the DB right now.

--- roster.php
+++ roster.php
@@ -1,70 +1,26 @@
 <?php
-	session_start();
-	require_once('common-definitions.php');
 
-	if (!isLoggedIn())
-	{
-		header('Location: index.php');
-		quit(0);
-	}
+session_start();
 
-	require_once('roster-common-functions.php');
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+require_once 'models/model.php';
 
-<html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<title>Roster - Team Manager</title>
-		<meta http-equiv="content-type" 
-			content="text/html;charset=utf-8" />
-		<link rel="stylesheet" type="text/css" href="styles/style.css" /> 
-	</head>
+if (!isLoggedIn())
+{
+	header('Location: index.php');
+	exit(0);
+}
 
-	<body id="roster-body">
-		<div id="container">
-			<div id="roster-header">
-				<h1>Roster</h1>
-			</div>
+require_once 'models/roster.php';
 
-<?php
-	include('includes/navbar.php');
-?>
+if (isset($_GET['name']))
+{
+//TODO must get other parameters
+	$action = 'show-roster';
+	$roster = getRoster($_GET['name']);
+}
+else
+{
+	$action = 'show-team-list';
+}
 
-			<div id="roster-content">
-<?php
-	if (isset($_GET['name']))
-	{
-		$db_con = connectToDB();
-
-		$escapedTeamName = mysqli_real_escape_string($db_con, $_GET['name']);
-		$roster_query_result = mysqli_query($db_con, "SELECT DISTINCT TeamName, ParkName, FieldNum, DayOfWeek, SeasonDescription FROM Roster WHERE TeamName = '$escapedTeamName' ORDER BY SeasonDescription");
-//DEBUG
-if ( !$roster_query_result)
-	error_log("Roster query result was NULL");
-//END DEBUG
-?>
-				<ul>
-<?php
-		while ($leagueRow = mysqli_fetch_array($roster_query_result))
-		{
-?>
-					<!-- TODO fix this: <li><a href="roster.php?name=<? $_GET['name'] ?>& -->
-					<li><a href="#">TODO</a></li>
-<?php
-		}
-?>
-				</ul>
-
-<?php
-		closeDB($db_con);
-	}
-?>
-			</div> <!-- roster-content -->
-
-<?php
-	include('includes/footer.php');
-?>
-		</div> <!-- end container -->
-	</body>
-</html>
+require 'views/roster.php';
