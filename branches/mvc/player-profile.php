ca31aef021c6f5a4b955477267db14a9ac1dac73
mark <mark@96c4993e-0f1d-2d11-6717-7f96f89844b4>
2014-02-02 05:24:53 +0000

    Converted Player Profile page to new MVC model.
    Moved player-common-functions.php to models/player.php.
    
    Deleted common-definitions.php because it was long ago moved to models/model.php.

--- branches/mvc/player-profile.php
+++ branches/mvc/player-profile.php
@@ -1,110 +1,29 @@
 <?php
-	session_start();
-	require_once('common-definitions.php');
 
-	if (!isLoggedIn())
-	{
-		header('Location: index.php');
-		quit(0);
-	}
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+session_start();
 
-<html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-	<title>Player Profile - Team Manager</title>
-	<meta http-equiv="content-type" 
-		content="text/html;charset=utf-8" />
-	<link rel="stylesheet" type="text/css" href="styles/style.css" /> 
-<?php
-	require_once('player-common-functions.php');
-?>
-	</head>
+require_once 'models/model.php';
 
-	<body id="player-profile-body">
-		<div id="container">
-			<div id="player-profile-header">
-				<h1>Player Profile</h1>
-			</div> <!-- player-profile-header -->
-
-<?php
-	include('includes/navbar.php');
-?>
-
-			<div id="player-profile-content">
-<?php
-	$db_con = connectToDB();
-
-//TODO sanitize GET param
-	$db_query_result = mysqli_query($db_con, "SELECT * FROM Player WHERE Player.ID = ${_GET['id']}");
-//DEBUG
-// show an error if the query failed
-if ($db_query_result == NULL)
+//TODO redirect to the login page with a message that user can't view a player profile without being logged in
+if (!isLoggedIn())
 {
-  echo "<p class=\"db-error\">The query result was NULL :(</p>";
+	header('Location: index.php');
+	exit(0);
 }
-//END DEBUG
 
-	$playerInfo = mysqli_fetch_array($db_query_result);
-?>
-				<img title="<?= $playerInfo['FirstName'] . " " . $playerInfo['LastName'] ?>" src="images/player-no-image.gif" />
+require_once 'models/player.php';
 
-				<h2><?= $playerInfo['FirstName'] . " " . $playerInfo['LastName'] ?></h2>
-				<?= $playerInfo['NickName'] ? "<h3>\"${playerInfo['NickName']}\"</h3>" : "" ?>
+$playerInfo = getPlayerInfo($_GET['id']);
+//TODO if $playerInfo is NULL, then use an $action to show a query failure message
 
-				<p><span class="bold">Phone:</span> <?= $playerInfo['PhoneNumber'] ? formatPhoneNumber($playerInfo['PhoneNumber']) : '[Not Specified]' ?><br>
-				    <span class="bold">Email:</span> <?= $playerInfo['Email'] ? "<a href=\"mailto:${playerInfo['Email']}\">${playerInfo['Email']}</a>" : '[Not Specified]' ?><br>
-				    <span class="bold">Gender:</span> <?= $playerInfo['Gender'] ? $playerInfo['Gender'] : '[Not Specified]' ?></p>
+$firstName = $playerInfo['FirstName'];
+$lastName = $playerInfo['LastName'];
+$nickName = $playerInfo['NickName'];
+$phone = $playerInfo['PhoneNumber'] ? formatPhoneNumber($playerInfo['PhoneNumber']) : '[Not Specified]';
+$email = $playerInfo['Email'] ? '<a href="mailto:' . $playerInfo['Email'] . '">' . $playerInfo['Email'] . '</a>' : '[Not Specified]';
+$gender = $playerInfo['Gender'] ? $playerInfo['Gender'] : '[Not Specified]';
 
-<?php
-//TODO query DB for teams for teams this player manages or plays on and list them (look further below for initial attempt to do this)
-?>
-<!--TODO show nickname if exists 
-				<p><span class="bold"><?= $playerInfo['FirstName'] ?>'s current and past teams:<br />
-				    <ul>
-					<li> -->
-<?php
-/*
-	// print current/most recent teams in a table
-	echo "<div id=\"player_s-teams\">";
-	// get today's date	
-	//$today = date("Y-m-d");
-	// find all rows for the player in the players table. Players are duplicated as seasons pass; that's why this query is ugly.
-//TODO need a better way to find the player. Now, we are only checking first and last name. That is not unique enough. Is it okay to keep driver's license #? How about just a hash?
-	$db_query_result = mysqli_query($db_con, "SELECT * FROM players AS p1 JOIN players AS p2 ON p1.firstName = p2.firstName AND p1.lastName = p2.lastName JOIN teams ON p2.associatedTeam = teams.teamID JOIN leagues ON teams.associatedLeague = leagues.leagueID JOIN seasons ON leagues.associatedSeason = seasonID WHERE p1.playerID = " . $playerID);
-	$tableHeaderMustBePrinted = true;
-	while ($row = mysqli_fetch_array($db_query_result))
-	{
-//TODO it might be best if I just have a global variable for the current/next season
-//Previously, I wanted to have a function to find the latest season the player was playing in. I can still do that.
-		if ($row['seasonID'] == 1)
-		{
-			if ($tableHeaderMustBePrinted)
-			{
-				$tableHeaderMustBePrinted = false;		
-				echo "<p class=\"bold\">Currently playing in these divsions:</p>";
-				echo "<table><tr><th>Season</th><th>Division</th><th>Class</th><th>Park</th><th>Team</th></tr>";
-			}
+//TODO query DB for teams for teams this player manages or plays on and list them
+$teams = getPlayerTeams($_GET['id']);
 
-//TODO needs a day-of-week column
-			echo "<tr><td class=\"spring13\">" . $row['description'] . "</td><td>" . $row['division'] . "</td><td>" . $row['class'] . "</td><td>" . $row['park'] . "</td><td>" . $row['name'] . "</td></tr>";
-		}
-	}
-
-	// only close table if needed
-	if ( !$tableHeaderMustBePrinted)
-		echo "</table>";
-
-	echo "</div> <!-- player_s-teams -->";
-*/
-
-	closeDB($db_con);
-
-
-	include('includes/footer.php');
-?>
-			</div> <!-- player-profile-content -->
-		</div> <!-- end container -->
-	</body>
-</html>
+require 'views/player-profile.php';
