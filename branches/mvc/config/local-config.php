ad19f1eb4fc48b5961cff291a19087b218e705e7
mark <mark@96c4993e-0f1d-2d11-6717-7f96f89844b4>
2014-05-27 20:58:43 +0000

    Added copyright notices to all source files.

--- branches/mvc/config/local-config.php
+++ branches/mvc/config/local-config.php
@@ -1,5 +1,26 @@
 <?php
 
+/* *************************************************************************
+
+  This file is part of Team Manager.
+
+  Copyright © 2013 Mark Ross <krazkidd@gmail.com>
+
+  Team Manager is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Team Manager is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with Team Manager.  If not, see <http://www.gnu.org/licenses/>.
+  
+************************************************************************* */
+
 define('DEFAULT_SEASON', 1);
 
 /* database access vars */
