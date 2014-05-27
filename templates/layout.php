f541479c8d821dda24eaf7b80fd90ccfb8ea35ae
mark <mark@96c4993e-0f1d-2d11-6717-7f96f89844b4>
2014-05-27 20:45:00 +0000

    Added license file, license notice to About, and README file.

--- templates/layout.php
+++ templates/layout.php
@@ -1,6 +1,27 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
+<!-- *************************************************************************
+
+  This file is part of Team Manager.
+
+  Copyright © 2013 Mark Ross <krazkidd@gmail.com>
+
+  Team Manager is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Team Manager is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with Team Manager.  If not, see <http://www.gnu.org/licenses/>.
+  
+************************************************************************* -->
+
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<title><?php echo $title . ' - ' . $PROJECT_NAME ?></title>
@@ -38,7 +59,7 @@
 
 			<div id="footer">
 				<p><a href="/index.php">Home</a></p>
-				<p>Copyright &copy; 2014. Website design by Mark Ross.</p>
+				<p>Copyright &copy; 2013. Website design by Mark Ross.</p>
 			</div>
 		</div>
 	</body>
