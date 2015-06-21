702c582de1900e5f25cdcd024bdfcf6fb7314e1b
Mark Ross <krazkidd@gmail.com>
2015-06-21 00:30:41 -0700

    Oops. Forgot the JS file.

---
+++ js/team_mgr.js
@@ -0,0 +1,16 @@
+/* 
+ * runs *after* document is completely loaded
+ */
+window.onload = function() {
+    //alert("welcome");
+};
+
+/*
+ * runs *before* document is completely loaded
+ */
+$(document).ready(function() {
+    // add event handler to all links
+    //$("a").click(function( event ) {
+    //    alert("Thanks for visiting!");
+    //});
+});
